aef4d8d99b3239972327e79838d258fdecc1a166
Roman Khabarov <roman@globalart.dev>
2024-07-19 09:33:48 +0200

    fix

---
+++ prepare-scripts/init-dev-env.sh
@@ -0,0 +1,15 @@
+set -e
+
+DIR=/workspace
+PROJECT_NAME=test
+REPO_URL=git@github.com:GlobalArtInc/nestjs-boileraplate.git
+# cd $DIR
+mkdir -p /workspace
+cd $DIR
+
+if ! [ -d $DIR/$PROJECT_NAME ]; then
+    echo "Cloning repository..."
+    git clone $REPO_URL $PROJECT_NAME
+    cd $DIR/$PROJECT_NAME
+fi
+
