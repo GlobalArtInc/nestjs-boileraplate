b9e3466bf247e08c60677aa2cd45d31d62924e09
Roman Khabarov <roman@globalart.dev>
2024-07-19 08:15:51 +0000

    fix

--- app/apps/app-1/src/main.ts
+++ app/apps/app-1/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix('/api');
+
+  const config = new DocumentBuilder().setTitle('ETERNAL API').setVersion('2.0').build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api', app, document);
+
+
   await app.listen(3000);
 }
 bootstrap();
